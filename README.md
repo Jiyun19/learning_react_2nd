465d7d7e839cae660260ed6d8f860d7dfff2e9f0
Jiyun19 <jiyunlee219@gmail.com>
2022-01-27 05:54:06 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-r4a583
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-r4a583)
---
+++ index.tsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Hello from './Hello';
+import './style.css';
+
+//The const Keyword
+
+/*
+var div, container =document.getElementById("container");
+for (var i=0; i<5; i++){
+  div= document.createElement("div");
+  div.onclick =function(){
+    alert("This is box #" + i);
+  };
+  container.appendChild(div);
+}
+
+*/
+
+//The let Keyword
+
+/*var div, container =document.getElementById("container");
+
+for (let i=0; i<5; i++){
+  div =document.createElement("div");
+  div.onclick=function(){
+    alert("This is box#"+ i);
+  };
+  container.appendChild(div);
+}
+*/
+
+
