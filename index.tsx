a7cb78ae531a44f4f7652765f1dc3f9273bd7d9d
Jiyun19 <jiyunlee219@gmail.com>
2022-01-28 08:28:38 +0900

    1 file modified

--- index.tsx
+++ index.tsx
@@ -1,3 +1,5 @@
+// https://github.com/MoonHighway/learning-react
+
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
@@ -30,4 +32,45 @@ for (let i=0; i<5; i++){
 }
 */
 
+/*
+document.body.innerHTML = `
+<section>
+<header>
+<h1>The React Blog</h1>
+</header>
+<article>
+<h2>${article.title}</h2>
+${article.body}
+</article>
+<footer>
+<p>copyright ${new Date().getYear()} | The React Blog</p>
+</footer>
+</section>
+`;
+*/
 
+function logCompliment() {
+  console.log("You're doing great!");
+}
+
+logCompliment();
+
+const logCompliment2 = function () {
+  console.log("You're doing great!");
+};
+
+logCompliment2();
+
+//#PASSING ARGUMENTS
+
+const logCompliment3 =function(firstname){
+  console.log(`You're doing great, ${firstname}`);
+};
+
+logCompliment3("Molly!");
+
+const logCompliment4 =function(firstname, message){
+  console.log(`${message}, ${firstname}`);
+};
+
+logCompliment4("Molly!", "Hello~");
